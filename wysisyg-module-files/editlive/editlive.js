359c4e45265dc8f3a26e045c24616c9229bdd805
Michael Fromin <mfromin@yahoo.com>
2015-05-07 11:03:30 -0600

    Clean up mix of tabs and spaces in project files.

--- wysisyg-module-files/editlive/editlive.js
+++ wysisyg-module-files/editlive/editlive.js
@@ -6,27 +6,27 @@
    * Attach this editor to a target element.
    */
   Drupal.wysiwyg.editor.attach.editlive = function(context, params, settings) {
-    // alert('Attaching...');
-    ta = context.getElementById(params.field);
-    ta.style.display = "none";
+	// alert('Attaching...');
+	ta = context.getElementById(params.field);
+	ta.style.display = "none";
 
-    var div = context.createElement("div");
-    var divIdName = 'EL_' + Date.now();
-    div.setAttribute('id',divIdName);
-    div.style.width = "100%";
-    div.style.height = "600px";
+	var div = context.createElement("div");
+	var divIdName = 'EL_' + Date.now();
+	div.setAttribute('id',divIdName);
+	div.style.width = "100%";
+	div.style.height = "600px";
 
-    ta.parentElement.insertBefore(div, ta);
-    editor = new EditLiveJava('editlive', '100%', '100%');
-    // This is calculated in the editlive.inc file and referenced here
-    editor.setDownloadDirectory(settings.downloaddir);
-    editor.setConfigurationFile(settings.downloaddir + "/configuration.xml")
-    editor.setBody(encodeURIComponent(ta.value));
-    //Pass in the Drupal session cookie - its HTTP Only so we have to parse this on the server
-    editor.setCookie(settings.session_cookie);
-    // This is calculated in the editlive.inc file and referenced here
-    editor.setStyles(settings.editorcss);
-    editor.showInElement(divIdName);
+	ta.parentElement.insertBefore(div, ta);
+	editor = new EditLiveJava('editlive', '100%', '100%');
+	// This is calculated in the editlive.inc file and referenced here
+	editor.setDownloadDirectory(settings.downloaddir);
+	editor.setConfigurationFile(settings.downloaddir + "/configuration.xml")
+	editor.setBody(encodeURIComponent(ta.value));
+	//Pass in the Drupal session cookie - its HTTP Only so we have to parse this on the server
+	editor.setCookie(settings.session_cookie);
+	// This is calculated in the editlive.inc file and referenced here
+	editor.setStyles(settings.editorcss);
+	editor.showInElement(divIdName);
   };
 
   /**
@@ -35,16 +35,16 @@
    * See Drupal.wysiwyg.editor.detach.none() for a full description of this hook.
    */
   Drupal.wysiwyg.editor.detach.editlive = function (context, params, trigger) {
-    alert('Detaching...params.name:' + params.name)
-    // Needed to add the true parameter to this to ensure that local images were
-    // uploaded before the editor sends the body content to the server
-    ta.value = editor.getBody(true);
+	alert('Detaching...params.name:' + params.name)
+	// Needed to add the true parameter to this to ensure that local images were
+	// uploaded before the editor sends the body content to the server
+	ta.value = editor.getBody(true);
 
-    // We need to "dispose" of the editor here
-    // TODO: DELETE content button fails to work when EditLive is on the page
-    $("#EditLiveDiv").remove();
-    editor = null;
-    ta.style.display = "block";
+	// We need to "dispose" of the editor here
+	// TODO: DELETE content button fails to work when EditLive is on the page
+	$("#EditLiveDiv").remove();
+	editor = null;
+	ta.style.display = "block";
   };
 
   /**
@@ -53,17 +53,17 @@
    * Are there other uses?
    */
   Drupal.wysiwyg.editor.instance.editlive = {
-    insert: function (content) {
-      // alert('Insert');
-      editor.insertHTMLAtCursor(content);
-    },
-    setContent: function (content) {
-      // alert('Set Content');
-      editor.setBody(content);
-    },
-    getContent: function () {
-      // alert('Get Content');
-      return editor.getBody(true);
-    }
+	insert: function (content) {
+	  // alert('Insert');
+	  editor.insertHTMLAtCursor(content);
+	},
+	setContent: function (content) {
+	  // alert('Set Content');
+	  editor.setBody(content);
+	},
+	getContent: function () {
+	  // alert('Get Content');
+	  return editor.getBody(true);
+	}
   };
 })(jQuery);
--- wysisyg-module-files/textbox.io/textboxio-1.js
+++ wysisyg-module-files/textbox.io/textboxio-1.js
@@ -1,125 +1,125 @@
 (function ($) {
 
-    var defaultConfig = {
-        basePath: '/sites/all/libraries/textboxio/resources', // Default basePath is inferred from script location which fails in Drupal
-        css: {
-            stylesheets: [''],
-            styles: [
-                {
-                    rule: 'p',
-                    text: 'block.p'
-                },
-                {
-                    rule: 'h1',
-                    text: 'block.h1'
-                },
-                {
-                    rule: 'h2',
-                    text: 'block.h2'
-                },
-                {
-                    rule: 'h3',
-                    text: 'block.h3'
-                },
-                {
-                    rule: 'h4',
-                    text: 'block.h4'
-                },
-                {
-                    rule: 'div',
-                    text: 'block.div'
-                },
-                {
-                    rule: 'pre',
-                    text: 'block.pre'
-                }
-            ]
-        },
-        codeview: {
-            enabled: true,
-            showButton: true
-        },
-        images: {
-            upload: {
-                url: '/ephox_image_upload/ephox_process_image'
-            },
-            allowLocal: true
-        },
-        languages: ['en', 'es', 'fr', 'de', 'pt', 'zh'],
-        // locale : '', // Default locale is inferred from client browser
-        paste: {
-            style: 'prompt'
-        },
-        // spelling : {},
-        ui: {
-            toolbar: {
-                items: ['undo', 'insert', 'style', 'emphasis', 'align', 'listindent', 'format', 'tools']
-            }
-        }
-    };
+	var defaultConfig = {
+		basePath: '/sites/all/libraries/textboxio/resources', // Default basePath is inferred from script location which fails in Drupal
+		css: {
+			stylesheets: [''],
+			styles: [
+				{
+					rule: 'p',
+					text: 'block.p'
+				},
+				{
+					rule: 'h1',
+					text: 'block.h1'
+				},
+				{
+					rule: 'h2',
+					text: 'block.h2'
+				},
+				{
+					rule: 'h3',
+					text: 'block.h3'
+				},
+				{
+					rule: 'h4',
+					text: 'block.h4'
+				},
+				{
+					rule: 'div',
+					text: 'block.div'
+				},
+				{
+					rule: 'pre',
+					text: 'block.pre'
+				}
+			]
+		},
+		codeview: {
+			enabled: true,
+			showButton: true
+		},
+		images: {
+			upload: {
+				url: '/ephox_image_upload/ephox_process_image'
+			},
+			allowLocal: true
+		},
+		languages: ['en', 'es', 'fr', 'de', 'pt', 'zh'],
+		// locale : '', // Default locale is inferred from client browser
+		paste: {
+			style: 'prompt'
+		},
+		// spelling : {},
+		ui: {
+			toolbar: {
+				items: ['undo', 'insert', 'style', 'emphasis', 'align', 'listindent', 'format', 'tools']
+			}
+		}
+	};
 
-    // Attach this editor to target element
-    Drupal.wysiwyg.editor.attach.textboxio = function (context, params, settings) {
-        var ta = context.getElementById(params.field);
-        ta.style.display = "none";
-        var div = context.createElement("div");
-        var divIdName = 'tbio_id' + Date.now();
-        div.setAttribute('id', divIdName);
-        div.style.width = "100%";
-        div.style.height = "400px";
-        ta.detach_editor = function () {
-            var e = textboxio.get("#" + divIdName);
-            e[0].restore();
-            $('#' + divIdName).remove();
-            e[0] = null;
-        };
-        ta.parentElement.insertBefore(div, ta);
-        var editor = textboxio.replace('#' + divIdName, defaultConfig);
-        var contentToSet = (ta.value);
-        editor.content.set(contentToSet);
-        ta.get_editor = function () {
-            return editor;
-        };
-    };
+	// Attach this editor to target element
+	Drupal.wysiwyg.editor.attach.textboxio = function (context, params, settings) {
+		var ta = context.getElementById(params.field);
+		ta.style.display = "none";
+		var div = context.createElement("div");
+		var divIdName = 'tbio_id' + Date.now();
+		div.setAttribute('id', divIdName);
+		div.style.width = "100%";
+		div.style.height = "400px";
+		ta.detach_editor = function () {
+			var e = textboxio.get("#" + divIdName);
+			e[0].restore();
+			$('#' + divIdName).remove();
+			e[0] = null;
+		};
+		ta.parentElement.insertBefore(div, ta);
+		var editor = textboxio.replace('#' + divIdName, defaultConfig);
+		var contentToSet = (ta.value);
+		editor.content.set(contentToSet);
+		ta.get_editor = function () {
+			return editor;
+		};
+	};
 
-    /**
-     * Detach a single or all editors.
-     *
-     * See Drupal.wysiwyg.editor.detach.none() for a full description of this hook.
-     */
+	/**
+	 * Detach a single or all editors.
+	 *
+	 * See Drupal.wysiwyg.editor.detach.none() for a full description of this hook.
+	 */
 
-    Drupal.wysiwyg.editor.detach.textboxio = function (context, params, trigger) {
-        //alert('detach');
-        var ta = context.getElementById(params.field);
-        var editor = ta.get_editor();
-        ta.value = editor.content.get();
-        ta.detach_editor();
-        ta.style.display = "block";
-    };
+	Drupal.wysiwyg.editor.detach.textboxio = function (context, params, trigger) {
+		//alert('detach');
+		var ta = context.getElementById(params.field);
+		var editor = ta.get_editor();
+		ta.value = editor.content.get();
+		ta.detach_editor();
+		ta.style.display = "block";
+	};
 
-    Drupal.wysiwyg.editor.instance.textboxio = {
+	Drupal.wysiwyg.editor.instance.textboxio = {
 
-        insert: function (content) {
-            //alert('Insert');
-            var ta = context.getElementById(params.field);
-            var editor = ta.get_editor();
-            editor.content.insertHtmlAtCursor(content);
-        },
+		insert: function (content) {
+			//alert('Insert');
+			var ta = context.getElementById(params.field);
+			var editor = ta.get_editor();
+			editor.content.insertHtmlAtCursor(content);
+		},
 
-        setContent: function (content) {
-            //alert('Set Content');
-            var ta = context.getElementById(params.field);
-            var editor = ta.get_editor();
-            editor.content.set(content);
-        },
+		setContent: function (content) {
+			//alert('Set Content');
+			var ta = context.getElementById(params.field);
+			var editor = ta.get_editor();
+			editor.content.set(content);
+		},
 
-        getContent: function () {
-            //alert('Get Content');
-            var ta = context.getElementById(params.field);
-            var editor = ta.get_editor();
-            return editor.content.get();
-        }
+		getContent: function () {
+			//alert('Get Content');
+			var ta = context.getElementById(params.field);
+			var editor = ta.get_editor();
+			return editor.content.get();
+		}
 
-    };
+	};
 
 })(jQuery);
